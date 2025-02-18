b5fe4b1d364e41a4fb0ffe4fe08ba2b091143b1c
Inna Ianko <inna.i@mate.academy>
2025-02-18 11:58:15 +0200

    Fixed typo

--- tests/signIn/signInPositive.spec.js
+++ tests/signIn/signInPositive.spec.js
@@ -1,29 +1,28 @@
-import { test } from '@playwright/test';
-import { SignInPage } from '../../src/pages/SignInPage'; 
-import { HomePage } from '../../src/pages/HomePage'; 
+import { test } from "@playwright/test";
+import { SignInPage } from "../../src/pages/SignInPage";
+import { HomePage } from "../../src/pages/HomePage";
 
-test.describe('Sign up positive tests', () => {
+test.describe("Sign in positive tests", () => {
   let signInPage;
   let homePage;
   let user;
- 
-  test.beforeEach( async ({ page }) => {
-     signInPage = new SignInPage(page);
-     homePage = new HomePage(page);
+
+  test.beforeEach(async ({ page }) => {
+    signInPage = new SignInPage(page);
+    homePage = new HomePage(page);
 
     user = {
-    email: 'test_new_user@gmail.com',
-    password: 'newpass123!',
-     }
+      email: "test_new_user@gmail.com",
+      password: "newpass123!",
+    };
   });
 
-test('Successful `Sign in` flow test', async ({ page }) => {
-  await signInPage.open();
-  await signInPage.fillEmailField(user.email);
-  await signInPage.fillPasswordField(user.password);
-  await signInPage.clickSignInButton(); 
+  test("Successful `Sign in` flow test", async ({ page }) => {
+    await signInPage.open();
+    await signInPage.fillEmailField(user.email);
+    await signInPage.fillPasswordField(user.password);
+    await signInPage.clickSignInButton();
 
-  await homePage.assertYourFeedTabIsVisible();
-});
-
+    await homePage.assertYourFeedTabIsVisible();
+  });
 });
