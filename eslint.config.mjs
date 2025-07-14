7dc11b52654d867a30d83c33972f1f70d2d0435b
sr.veremienko@gmail.com <sr.veremienko@gmail.com>
2025-07-14 19:34:21 +0300

    fix collection according to prettire and eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,38 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import { defineConfig } from 'eslint/config';
+import pluginJs from '@eslint/js';
+import playwright from 'eslint-plugin-playwright';
+import eslintConfigPrettier from 'eslint-config-prettier';
+
+export default defineConfig([
+  {
+    files: ['**/*.{js,mjs,cjs}'],
+    plugins: { js },
+    extends: ['js/recommended'],
+  },
+  eslintConfigPrettier,
+  { files: ['**/*.{js,mjs,cjs}'], languageOptions: { globals: globals.node } },
+  {
+    ...pluginJs.configs.recommended,
+    ...playwright.configs['flat/recommended'],
+    rules: {
+      ...pluginJs.configs.recommended.rules,
+      'no-unused-vars': 'error',
+      'max-len': [
+        'error',
+        {
+          code: 80,
+          comments: 80,
+        },
+      ],
+      ...playwright.configs['flat/recommended'].rules,
+      'playwright/expect-expect': 'off',
+    },
+    ignores: [
+      '**/node_modules/*',
+      'playwright.config.js',
+      '**/playwright-report/**',
+    ],
+  },
+]);
